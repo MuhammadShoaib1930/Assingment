5a8edc07a3d06b641e925cf282d530aa0717d114
Muhamma-Shoaib <shoaibsn54321@gmail.com>
2024-03-31 03:03:04 +0500

    Modifing ThreadDemo.java and Removing bugs of RunnableDemo.java

--- RunnableDemo.java
+++ RunnableDemo.java
@@ -1,44 +1,20 @@
 package Assingment;
 
-// public class RunnableDemo {
-//     public static void main(String[] args) {
-//         MyRunnable obj1 = new MyRunnable();
-//         Thread thread1 = new Thread(obj1);
-//         thread1.start();
-//         obj1 = new MyRunnable();
-//         thread1 = new Thread(obj1);
-//         thread1.start();
-//     }
-// }
-// class MyRunnable implements Runnable{
-//     public void run(){
-//         for(int i = 0; i<10;i++){
-//             System.out.println("Hello, I am a runnable!");
-//         }
-//     }
-// }
-
 public class RunnableDemo {
     public static void main(String[] args) {
         MyRunnable obj1 = new MyRunnable();
         Thread thread1 = new Thread(obj1);
         thread1.start();
-        System.out.println(obj1.sharedCounter);
         obj1 = new MyRunnable();
         thread1 = new Thread(obj1);
         thread1.start();
-        System.out.println(obj1.sharedCounter);
     }
 }
 class MyRunnable implements Runnable{
-    public static int sharedCounter = 0;
     public void run(){
-        if(sharedCounter<0){
-            return;
+        for(int i = 0; i<10;i++){
+            System.out.println("Hello, I am a runnable!");
         }
-        for(int i = 0; i<=sharedCounter;i++){
-            sharedCounter++;
-        }
-        System.out.println(sharedCounter);
     }
 }
+
--- ThreadDemo.java
+++ ThreadDemo.java
@@ -1,18 +1,52 @@
 package Assingment;
 
-public interface ThreadDemo {
+// public interface ThreadDemo {
+//     public static void main(String[] args) {
+//         MyThread obj1 = new MyThread();
+//         obj1.start();
+//         obj1 = new MyThread();
+//         obj1.start();
+//     }
+// }
+// class MyThread extends Thread{
+//     @Override
+//     public void run(){
+//         for(int i = 0; i<10;i++){
+//             System.out.println("Hello, I am a thread! ");
+//         }
+//     }
+// }
+
+public class ThreadDemo {
     public static void main(String[] args) {
-        MyThread obj1 = new MyThread();
-        obj1.start();
-        obj1 = new MyThread();
-        obj1.start();
+        MyRunnable obj1 = new MyRunnable();
+        MyRunnable obj2 = new MyRunnable();
+        Thread thread1 = new Thread(obj1);
+        Thread thread2 = new Thread(obj2);
+        thread1.start();
+        thread2.start();
+
+        //Handling Interrupted Exception
+        try {
+            thread1.join(); //waiting for thread1 to complete.
+            thread2.join();//waiting for thread2 to complete.
+        } catch (InterruptedException var) {
+            System.out.println("Threads Interrupts for waiting... ");
+        }
+        System.out.println(MyRunnable.getCounter());
     }
 }
-class MyThread extends Thread{
-    @Override
+class MyRunnable implements Runnable{
+    private static int sharedCounter = 0;
+    private static final Object lock = new Object();
     public void run(){
         for(int i = 0; i<10;i++){
-            System.out.println("Hello, I am a thread! ");
+            synchronized (lock){
+                sharedCounter++;
+            }
         }
+    }
+    public static int getCounter(){
+        return sharedCounter;
     }
 }
